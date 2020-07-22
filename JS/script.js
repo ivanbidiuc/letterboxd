5153132bc890f5ab2a027fed54b5a3323dfef4bb
Ivan <ionbidiuc@gmail.com>
2020-07-22 10:13:53 +0300

    test

--- JS/script.js
+++ JS/script.js
@@ -9,21 +9,24 @@ $(document).ready(() => {
   
   const getInitialMovies = async () => {
     const BASE_URL =
-      "https://api.themoviedb.org/3/dicover/movie?api_key=a89b0700b3e3de04639603191d2a8a5a'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+      "https://api.themoviedb.org/3/discover/movie?api_key=a89b0700b3e3de04639603191d2a8a5a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1 ";
     try {
       const data = await axios.get(BASE_URL);
-      const movies = data.data.results
-        .splice(0, 13)
-        .reduce((result, item, key) => {
-          if (key === 1)
-            return `
-          <div class="content-movies-movies-movie" onClick="setMovieToStorage(${item.id})">
-            <a href="./movie.html"> 
-              <img class="content-movies-movies-movie--img" src="https://image.tmdb.org/t/p/w500${item.poster_path}"
-              alt="" />
-            </a>
-          </div>`;
-          // result pentru prima iteratie este [object, object]
+      const movies = data.data.results;
+      
+
+        let movies = "";
+    for (let i = 0; i < 12; i++) {
+      movies += `
+      <div class="content-movies__movies__movie" onClick="setMovieToStorage(${data.data.results[i].id})">
+          <a href="./movie.html"> 
+            <img class="content-movies__movies__movie--img" src="https://image.tmdb.org/t/p/w500${data.data.results[i].poster_path}"
+            alt="" />
+          </a>
+        </div>
+      `;
+    }
+          
           return `${result}
             <div class="content-movies-movies-movie" onClick="setMovieToStorage(${item.id})">
               <a href="index.html"> 
